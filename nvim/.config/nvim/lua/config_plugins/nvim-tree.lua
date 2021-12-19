18000394a029d9fb6c312415cf8b0a8881f926cf
Ki-Tae Kim <kkim107@ucmerced.edu>
2021-12-18 17:22:58 -0800

    add alpha

--- nvim/.config/nvim/lua/config_plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/config_plugins/nvim-tree.lua
@@ -1,3 +1,8 @@
+local status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not status_ok then
+  return
+end
+
 vim.g.nvim_tree_show_icons = {
   git = 1,
   folders = 1,
@@ -9,8 +14,12 @@ vim.g.nvim_tree_icons = {
   default = "",
   symlink = "",
   git = {
+    unstaged = "",
+    staged = "S",
     unmerged = "",
     deleted = "",
+    untracked = "U",
+    ignored = "◌",
   },
   folder = {
     default = "",
@@ -26,6 +35,9 @@ vim.g.nvim_tree_respect_buf_cwd = 1
 require'nvim-tree'.setup {
   auto_close = true,
   update_cwd = true,
+  ignore_ft_on_setup = {
+    "alpha",
+  },
   update_focused_file = {
     enable = true,
     update_cwd = true
