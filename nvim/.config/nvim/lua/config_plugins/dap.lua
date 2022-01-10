fe9fa03a08a837c1c73138ecf8020e3439d79077
Ki-Tae Kim <kkim107@ucmerced.edu>
2022-01-10 15:29:30 -0800

    add some insert keymappings

--- nvim/.config/nvim/lua/config_plugins/dap.lua
+++ nvim/.config/nvim/lua/config_plugins/dap.lua
@@ -4,9 +4,13 @@ vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''}
 vim.fn.sign_define('DapStopped', {text='', texthl='', linehl='', numhl=''})
 
 vim.api.nvim_set_keymap('n', '<F5>', ":lua require'dap'.continue()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap('i', '<F5>', "<cmd>:lua require'dap'.continue()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<F10>', ":lua require'dap'.step_over()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap('i', '<F10>', "<cmd>:lua require'dap'.step_over()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<F11>', ":lua require'dap'.step_into()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap('i', '<F11>', "<cmd>:lua require'dap'.step_into()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<F12>', ":lua require'dap'.step_out()<CR>", {noremap=true, silent=true})
+vim.api.nvim_set_keymap('i', '<F12>', "<cmd>:lua require'dap'.step_out()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<Leader>db', ":lua require'dap'.toggle_breakpoint()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<Leader>dr', ":lua require'dap'.repl.open()<CR>", {noremap=true, silent=true})
 vim.api.nvim_set_keymap('n', '<Leader>dc', ":lua require'dap'.run_to_cursor()<CR>", {noremap=true, silent=true})
