9dd9f9f11669d6ffabf283b43e544700ee8d3b57
ktkimit <qlsn55@gmail.com>
2023-06-16 22:36:19 -0400

    [nvim] Add indent_blankline.lua.

---
+++ nvim/.config/nvim/lua/plugin/indent_blankline.lua
@@ -0,0 +1,18 @@
+local M = {
+  "lukas-reineke/indent-blankline.nvim",
+  event = "BufReadPre",
+}
+
+M.opts = {
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = true,
+  use_treesitter = true,
+  show_current_context = true,
+  buftype_exclude = { "terminal", "nofile" },
+  filetype_exclude = {
+    "help",
+    "NvimTree",
+  },
+}
+
+return M
