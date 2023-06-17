7686e9da8edb107b88297ab6147dd91716ada00a
ktkimit <qlsn55@gmail.com>
2023-06-16 22:43:02 -0400

    [nvim] Update telescope.lua and lazy-configure.lua.

--- nvim/.config/nvim/lua/lazy-configure.lua
+++ nvim/.config/nvim/lua/lazy-configure.lua
@@ -12,7 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- example using a list of specs with the default options
-vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+-- vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
 
 -- load lazy
 require("lazy").setup("plugin")
--- nvim/.config/nvim/lua/plugin/telescope.lua
+++ nvim/.config/nvim/lua/plugin/telescope.lua
@@ -5,7 +5,6 @@ local M = {
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { "ahmedkhalf/project.nvim" },
   },
 }
 
