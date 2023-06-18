e1105506c8caab1b40ada424859ce70faeb1c39c
ktkimit <qlsn55@gmail.com>
2023-06-18 16:38:54 -0400

    [nvim] Add dap-ui.lua.

---
+++ nvim/.config/nvim/lua/plugin/dap-ui.lua
@@ -0,0 +1,16 @@
+local M = {
+  "rcarriga/nvim-dap-ui",
+  event = "VeryLazy",
+  dependencies = {
+    {
+      "mfussenegger/nvim-dap",
+      event = "VeryLazy",
+    },
+  },
+}
+
+function M.config()
+  require("dapui").setup {}
+end
+
+return M
