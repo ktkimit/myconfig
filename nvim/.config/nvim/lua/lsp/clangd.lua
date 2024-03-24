0ba350df8124a6d3b9b05dde3c797dd52a519f7f
ktkimit <qlsn55@gmail.com>
2024-03-24 03:55:36 -0400

    Correct multiple different .. error.

--- nvim/.config/nvim/lua/lsp/clangd.lua
+++ nvim/.config/nvim/lua/lsp/clangd.lua
@@ -3,6 +3,7 @@ return{
     "clangd",
     "--background-index",
     "--suggest-missing-includes",
-    "--function-arg-placeholders"
+    "--function-arg-placeholders",
+    "--offset-encoding=utf-16"
   }
 }
