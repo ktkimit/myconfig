992b8a27a27603dc130a880299dd10f87a9252f9
ktkimit <qlsn55@gmail.com>
2024-06-21 13:25:38 -0400

    Add switching between source and header files in cpp dev.

--- nvim/.config/nvim/lua/lsp/clangd.lua
+++ nvim/.config/nvim/lua/lsp/clangd.lua
@@ -1,9 +1,14 @@
-return{
+return {
   cmd = {
     "clangd",
     "--background-index",
     "--suggest-missing-includes",
     "--function-arg-placeholders",
     "--offset-encoding=utf-16"
+  },
+  settings = {
+    on_attach = {
+      vim.keymap.set('n', '<space>a', "<cmd>ClangdSwitchSourceHeader<CR>")
+    }
   }
 }
