55a3bd6fcd10213fbd6dee04d4a9815e47290cd9
ktkimit <qlsn55@gmail.com>
2024-08-03 11:32:35 -0400

    Show buffer id in bufferline.

--- nvim/.config/nvim/lua/plugin/bufferline.lua
+++ nvim/.config/nvim/lua/plugin/bufferline.lua
@@ -12,7 +12,7 @@ function M.config()
   require("bufferline").setup{
     options = {
       mode = "buffers",
-      numbers = "none",
+      numbers = "buffer_id",
       close_command = "bdelete! %d",
       right_mouse_command = "bdelete! %d",
       left_mouse_command = "buffer %d",
