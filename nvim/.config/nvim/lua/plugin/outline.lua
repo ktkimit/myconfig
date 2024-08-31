87c167f10e24f5f8c22a71173c3d7304d13cea96
ktkimit <qlsn55@gmail.com>
2024-08-31 11:36:54 -0400

    Add outline.

---
+++ nvim/.config/nvim/lua/plugin/outline.lua
@@ -0,0 +1,17 @@
+local M = {
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline", "OutlineOpen" },
+  keys = { -- Example mapping to toggle outline
+    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+  opts = {
+    -- Your setup opts here
+  },
+}
+
+function M.config()
+  require("outline").setup({})
+end
+
+return M
