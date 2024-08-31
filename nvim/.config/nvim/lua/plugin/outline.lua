2eddcb9f94b286fdbae400986a88d11e13117d43
ktkimit <qlsn55@gmail.com>
2024-08-31 12:36:57 -0400

    update outline config.

--- nvim/.config/nvim/lua/plugin/outline.lua
+++ nvim/.config/nvim/lua/plugin/outline.lua
@@ -3,7 +3,7 @@ local M = {
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
   keys = { -- Example mapping to toggle outline
-    { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    { "<space>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
   },
   opts = {
     -- Your setup opts here
