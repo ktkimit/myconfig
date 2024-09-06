2a45b1689cfdd6ee4970b42c96858a87a245d1a6
ktkimit <qlsn55@gmail.com>
2024-09-06 05:18:09 -0400

    Update todo-comments.

--- nvim/.config/nvim/lua/plugin/todo-comments.lua
+++ nvim/.config/nvim/lua/plugin/todo-comments.lua
@@ -4,7 +4,14 @@ local M = {
   event = "VeryLazy",
 }
 
-M.opts = {
-}
+function M.config()
+  vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
+  end, { desc = "Next todo comment" })
+
+  vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+  end, { desc = "Previous todo comment" })
+end
 
 return M
