d4b7b06a1c08f2f95ed8f979c7d02c6e4831d653
ktkimit <qlsn55@gmail.com>
2024-09-08 00:02:03 -0400

    Fix todo-comments not loaded issue.

--- nvim/.config/nvim/lua/plugin/todo-comments.lua
+++ nvim/.config/nvim/lua/plugin/todo-comments.lua
@@ -2,9 +2,12 @@ local M = {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   event = "VeryLazy",
+  opts = {}
 }
 
 function M.config()
+  require("todo-comments").setup()
+
   vim.keymap.set("n", "]t", function()
     require("todo-comments").jump_next()
   end, { desc = "Next todo comment" })
