f8df9d259b14207db03b04ff42755860a0a36160
ktkimit <qlsn55@gmail.com>
2024-09-13 15:02:12 -0400

    Add show symbol info of clangd.

--- nvim/.config/nvim/lua/lsp/clangd.lua
+++ nvim/.config/nvim/lua/lsp/clangd.lua
@@ -8,7 +8,8 @@ return {
   },
   settings = {
     on_attach = {
-      vim.keymap.set('n', '<space>a', "<cmd>ClangdSwitchSourceHeader<CR>"),
+      vim.keymap.set('n', '<space>a', "<cmd>ClangdSwitchSourceHeader<CR>", { desc = "Switch source and header." }),
+      vim.keymap.set('n', '<space>s', "<cmd>ClangdShowSymbolInfo<CR>", { desc = "Show symbol info." }),
       vim.lsp.inlay_hint.enable(true)
     }
   }
