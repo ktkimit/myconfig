752cc8585a7d01232228c4dba31ba7e32ef9f880
ktkimit <qlsn55@gmail.com>
2024-10-14 11:11:13 -0400

    Update telescope

--- nvim/.config/nvim/lua/plugin/telescope.lua
+++ nvim/.config/nvim/lua/plugin/telescope.lua
@@ -39,6 +39,22 @@ M.opts = {
 function M.config()
   local telescope = require('telescope')
 
+  local lga_actions = require("telescope-live-grep-args.actions")
+  telescope.setup {
+    extensions = {
+      live_grep_args = {
+        auto_quoting = true, -- enable/disable auto-quoting
+        -- define mappings, e.g.
+        mappings = {         -- extend mappings
+          i = {
+            ["<C-k>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          },
+        },
+      }
+    }
+  }
+
   telescope.load_extension('fzf')
   telescope.load_extension("live_grep_args")
   telescope.load_extension("undo")
