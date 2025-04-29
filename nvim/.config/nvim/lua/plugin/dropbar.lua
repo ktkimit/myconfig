2e5faf10e2d9c296c47ae8fbacea9ae6ab78ba37
ktkimit <qlsn55@gmail.com>
2025-04-28 22:21:45 -0400

    Change the setting of dropbar.

--- nvim/.config/nvim/lua/plugin/dropbar.lua
+++ nvim/.config/nvim/lua/plugin/dropbar.lua
@@ -2,57 +2,63 @@ local M = {
   'Bekaboo/dropbar.nvim',
   -- optional, but required for fuzzy finder support
   dependencies = {
-    'nvim-telescope/telescope-fzf-native.nvim'
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make'
   }
 }
 
 function M.config()
   local dropbar = require("dropbar")
   dropbar.setup({
-    menu = {
-      keymaps = {
-        ['<S-Tab>'] = function()
-          local utils = require('dropbar.utils')
-          local menu = utils.menu.get_current()
-          if not menu then
-            return
-          end
-          if menu.prev_menu then
-            menu:close()
-          else
-            local bar = require('dropbar.utils.bar').get({ win = menu.prev_win })
-            local barComponents = bar.components[1]._.bar.components
-            for _, component in ipairs(barComponents) do
-              if component.menu then
-                local idx = component._.bar_idx
-                menu:close()
-                require('dropbar.api').pick(idx - 1)
-              end
-            end
-          end
-        end,
-        ['<Tab>'] = function()
-          local utils = require('dropbar.utils')
-          local menu = utils.menu.get_current()
-          if not menu then
-            return
-          end
-          local cursor = vim.api.nvim_win_get_cursor(menu.win)
-          local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
-          if component then
-            menu:click_on(component, nil, 1, 'l')
-          end
-        end,
-      }
-    }
+    -- menu = {
+    --   keymaps = {
+    --     ['<S-Tab>'] = function()
+    --       local utils = require('dropbar.utils')
+    --       local menu = utils.menu.get_current()
+    --       if not menu then
+    --         return
+    --       end
+    --       if menu.prev_menu then
+    --         menu:close()
+    --       else
+    --         local bar = require('dropbar.utils.bar').get({ win = menu.prev_win })
+    --         local barComponents = bar.components[1]._.bar.components
+    --         for _, component in ipairs(barComponents) do
+    --           if component.menu then
+    --             local idx = component._.bar_idx
+    --             menu:close()
+    --             require('dropbar.api').pick(idx - 1)
+    --           end
+    --         end
+    --       end
+    --     end,
+    --     ['<Tab>'] = function()
+    --       local utils = require('dropbar.utils')
+    --       local menu = utils.menu.get_current()
+    --       if not menu then
+    --         return
+    --       end
+    --       local cursor = vim.api.nvim_win_get_cursor(menu.win)
+    --       local component = menu.entries[cursor[1]]:first_clickable(cursor[2])
+    --       if component then
+    --         menu:click_on(component, nil, 1, 'l')
+    --       end
+    --     end,
+    --   }
+    -- }
   })
 
-  local dropbar_menu = require("dropbar.utils.menu")
-  vim.ui.select = dropbar_menu.select
+  -- local dropbar_menu = require("dropbar.utils.menu")
+  -- vim.ui.select = dropbar_menu.select
+  --
+  -- vim.keymap.set("n", "<space>b", function()
+  --   require("dropbar.api").pick(vim.v.count ~= 0 and vim.v.count)
+  -- end)
 
-  vim.keymap.set("n", "<space>b", function()
-    require("dropbar.api").pick(vim.v.count ~= 0 and vim.v.count)
-  end)
+  local dropbar_api = require('dropbar.api')
+  vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+  vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+  vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
 end
 
 return M
