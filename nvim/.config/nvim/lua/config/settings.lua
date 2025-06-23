ed765c7b5259c31b1134dc0475384d13141231fb
ktkimit <qlsn55@gmail.com>
2025-06-23 14:29:32 -0400

    Add lazy, gruvbox and basic settings.

---
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -0,0 +1,36 @@
+-- set global options
+vim.o.number = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.cursorline = true
+vim.o.colorcolumn = '80'
+vim.o.swapfile = false
+vim.o.writebackup = false
+vim.o.undofile = true
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.termguicolors = true
+vim.o.updatetime = 300
+vim.o.scrolloff = 8
+vim.o.completeopt = 'menuone,noselect'
+vim.o.timeoutlen = 500
+vim.o.signcolumn = 'yes'
+vim.o.spell = true
+vim.o.spelllang = 'en_us'
+vim.o.cino = 'g0 N-s'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- set global variables
+vim.g.tex_flavor = 'latex'
+
+-- set through vim command
+vim.cmd[[hi CursorLine guibg=#000000]]
+
+vim.lsp.set_log_level("off")
