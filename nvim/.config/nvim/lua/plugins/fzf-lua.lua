e8e257492a0b6c4c8b111b428ac2570a0c9e6753
ktkimit <qlsn55@gmail.com>
2025-07-12 14:59:24 -0400

    keymap for fzflua oldfiles.

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -5,6 +5,7 @@ local M = {
 
 function M.opts()
   vim.keymap.set("n", "<Leader>ff", ":lua require('fzf-lua').files()<CR>", { desc = "FzfLua files" })
+  vim.keymap.set("n", "<Leader>fo", ":lua require('fzf-lua').oldfiles()<CR>", { desc = "FzfLua oldfiles" })
   vim.keymap.set("n", "<Leader>fb", ":lua require('fzf-lua').buffers()<CR>", { desc = "FzfLua buffers" })
   vim.keymap.set("n", "<Leader>fg", ":lua require('fzf-lua').live_grep_glob()<CR>", { desc = "FzfLua live_grep_glob" })
   vim.keymap.set("n", "<Leader>fp", ":lua require('fzf-lua').grep_project()<CR>", { desc = "FzfLua grep_project" })
