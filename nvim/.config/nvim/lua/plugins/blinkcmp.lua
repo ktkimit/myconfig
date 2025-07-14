f07f9d8309f087d0b1253edadda294a8cf9bd978
ktkimit <qlsn55@gmail.com>
2025-07-13 21:39:50 -0400

    Add blinkcmp and noice.

---
+++ nvim/.config/nvim/lua/plugins/blinkcmp.lua
@@ -0,0 +1,11 @@
+local M = {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+  opts = {
+  }
+}
+
+return M
