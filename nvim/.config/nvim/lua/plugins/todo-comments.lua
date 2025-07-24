a61c195073b2866b71d391c8aeda45fb8bab29e2
ktkimit <qlsn55@gmail.com>
2025-07-23 21:40:00 -0400

    Update todo-comments.

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,26 +1,15 @@
 local M = {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {},
 
-  keys = {
-    {
-      "]t",
-      function()
-        require("todo-comments").jump_next()
-      end,
-      desc = "Next todo comment"
-    },
-    {
-      "[t",
-      function()
-        require("todo-comments").jump_prev()
-      end,
-      desc = "Previous todo comment"
-    },
+  vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next()
+  end, { desc = "Next todo comment" }),
 
-  },
-
-  opts = {}
+  vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+  end, { desc = "Previous todo comment" })
 }
 
 return M
