3b6e998e143ce4379d4eee22542fdf11f35e8974
ktkimit <qlsn55@gmail.com>
2025-08-02 11:44:58 -0400

    Update fzflua.

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -4,14 +4,14 @@ local M = {
 }
 
 function M.opts()
-  vim.keymap.set("n", "<Leader>ff", ":lua require('fzf-lua').files()<CR>", { desc = "FzfLua files" })
-  vim.keymap.set("n", "<Leader>fo", ":lua require('fzf-lua').oldfiles()<CR>", { desc = "FzfLua oldfiles" })
-  vim.keymap.set("n", "<Leader>fb", ":lua require('fzf-lua').buffers()<CR>", { desc = "FzfLua buffers" })
-  vim.keymap.set("n", "<Leader>fg", ":lua require('fzf-lua').live_grep()<CR>", { desc = "FzfLua live_grep" })
-  vim.keymap.set("n", "<leader>fr", ":lua require('fzf-lua').lsp_references()<CR>", { desc = "FzfLua lsp_references" })
-  vim.keymap.set("n", "<leader>fd", ":lua require('fzf-lua').lsp_definitions()<CR>", { desc = "FzfLua lsp_definitions" })
-  vim.keymap.set("n", "<Leader>fp", ":lua require('fzf-lua').grep_project()<CR>", { desc = "FzfLua grep_project" })
-  vim.keymap.set("n", "<F1>", ":lua require('fzf-lua').help_tags()<CR>", { desc = "FzfLua help_tags" })
+  vim.keymap.set("n", "<Leader>ff", ":FzfLua files<CR>", { silent=true, desc = "FzfLua files" })
+  vim.keymap.set("n", "<Leader>fo", ":FzfLua oldfiles<CR>", { silent=true, desc = "FzfLua oldfiles" })
+  vim.keymap.set("n", "<Leader>fb", ":FzfLua buffers<CR>", { silent=true, desc = "FzfLua buffers" })
+  vim.keymap.set("n", "<Leader>fg", ":FzfLua live_grep<CR>", { silent=true, desc = "FzfLua live_grep" })
+  vim.keymap.set("n", "<leader>fr", ":FzfLua lsp_references<CR>", { silent=true, desc = "FzfLua lsp_references" })
+  vim.keymap.set("n", "<leader>fd", ":FzfLua lsp_definitions<CR>", { silent=true, desc = "FzfLua lsp_definitions" })
+  vim.keymap.set("n", "<Leader>fp", ":FzfLua grep_project<CR>", { silent=true, desc = "FzfLua grep_project" })
+  vim.keymap.set("n", "<F1>", ":FzfLua help_tags<CR>", { silent=true, desc = "FzfLua help_tags" })
   return {}
 end
 
