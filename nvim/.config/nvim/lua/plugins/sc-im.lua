f08382b9e5f8f3ad4087eb1cb0b8b928234edb00
ktkimit <qlsn55@gmail.com>
2025-08-31 13:36:43 -0400

    Remove sc-im.lua.

--- nvim/.config/nvim/lua/plugins/sc-im.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'DAmesberger/sc-im.nvim',
-  lazy = true,
-  opts = {
-  },
-
-  vim.keymap.set("n", "<Leader>sc", ":lua require'sc-im'.open_in_scim()<CR>", { silent = true, desc = "Open in scim" }),
-}
