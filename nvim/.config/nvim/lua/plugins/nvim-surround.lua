dcab8d0fbc1c6cfc5475a6de0c20ebfbd6f49aac
ktkimit <qlsn55@gmail.com>
2026-03-03 20:39:28 -0500

    update nvim-surround

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -1,11 +1,12 @@
 local M = {
   "kylechui/nvim-surround",
   event = "VeryLazy",
-  opts = {
-    keymaps = {
-      visual = "gs"
-    }
-  },
+  vim.keymap.set("x", "gs", "<Plug>(nvim-surround-visual)", {
+    desc = "Add a surrounding pair around a visual selection",
+  }),
+  vim.keymap.set("x", "gS", "<Plug>(nvim-surround-visual-line)", {
+    desc = "Add a surrounding pair around a visual selection, on new lines",
+  })
 }
 
 return M
