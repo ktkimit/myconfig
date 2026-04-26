d976cc122d247d7aea1e21ecc652c78d2e128bc2
Ki-Tae Kim <qlsn55@gmail.com>
2026-04-26 10:11:51 -0400

    Refactor dependencies in markit.lua

--- nvim/.config/nvim/lua/plugins/markit.lua
+++ nvim/.config/nvim/lua/plugins/markit.lua
@@ -1,6 +1,14 @@
 return {
     '2kabhishek/markit.nvim',
-    dependencies = { '2kabhishek/pickme.nvim', 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        {
+            '2kabhishek/pickme.nvim',
+            opts = {
+                picker = 'fzf-lua',
+            },
+        },
+        'nvim-lua/plenary.nvim',
+    },
     opts = {},
     event = { 'BufReadPre', 'BufNewFile' },
 }
