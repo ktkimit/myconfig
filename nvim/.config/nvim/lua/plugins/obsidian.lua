9976d466b34a21901a94c299ee75244a5cf1eb43
ktkimit <qlsn55@gmail.com>
2026-05-09 17:37:56 -0400

    Make obsidian plugin enabled when workspace exists.

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,19 +1,31 @@
+local workspaces = {
+  {
+    name = "personal",
+    path = "~/doc/notes",
+  },
+}
+
+local function workspaces_exist()
+  for _, ws in ipairs(workspaces) do
+    if vim.fn.isdirectory(vim.fn.expand(ws.path)) == 1 then
+      return true
+    end
+  end
+  return false
+end
+
 local M = {
   "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   -- lazy = true,
   -- ft = "markdown",
   event = "VeryLazy",
+  cond = workspaces_exist,
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/doc/notes",
-      },
-    },
+    workspaces = workspaces,
     daily_notes = {
       folder = "daily"
     },
